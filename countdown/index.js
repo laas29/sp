e1b50478c4a783f06947a1216f74271149833b0c
Luis Arias <laas29@gmail.com>
2018-03-24 00:41:47 -0600

    indentation

--- countdown/index.js
+++ countdown/index.js
@@ -1,38 +1,38 @@
-(function(){
+(function() {
 
-	var finalDate = new Date(1528541081697);
-  var sNum, mNum, hNum, dNum;
-	
-  function getSeconds(newDate){
-  	return Math.floor((finalDate - newDate) / 1000)%60;
-  }
-  
-  function getMinutes(newDate){
-  	return Math.floor((finalDate - newDate) / 60000)%60;
-  }
-  
-  function getHours(newDate){
-  	return Math.floor((finalDate - newDate) / 3600000)%24;
-  }
-  
-  function getDays(newDate){
-  	return Math.floor((finalDate - newDate) / 86400000);
-  }
-  
-  function updateClock(){
-    var newDate = new Date();
-	  sNum.innerHTML = getSeconds(newDate);
-    mNum.innerHTML = getMinutes(newDate);
-    hNum.innerHTML = getHours(newDate);
-    dNum.innerHTML = getDays(newDate);
-  }
-    
-  window.onload = function(){
-  	sNum = document.querySelector(".seconds-container .number");
-    mNum = document.querySelector(".minutes-container .number");
-    hNum = document.querySelector(".hours-container .number");
-    dNum = document.querySelector(".days-container .number");
-    var interval = setInterval(updateClock, 1000);
-  };
-      
+    var finalDate = new Date(1528541081697);
+    var sNum, mNum, hNum, dNum;
+
+    function getSeconds(newDate) {
+        return Math.floor((finalDate - newDate) / 1000) % 60;
+    }
+
+    function getMinutes(newDate) {
+        return Math.floor((finalDate - newDate) / 60000) % 60;
+    }
+
+    function getHours(newDate) {
+        return Math.floor((finalDate - newDate) / 3600000) % 24;
+    }
+
+    function getDays(newDate) {
+        return Math.floor((finalDate - newDate) / 86400000);
+    }
+
+    function updateClock() {
+        var newDate = new Date();
+        sNum.innerHTML = getSeconds(newDate);
+        mNum.innerHTML = getMinutes(newDate);
+        hNum.innerHTML = getHours(newDate);
+        dNum.innerHTML = getDays(newDate);
+    }
+
+    window.onload = function() {
+        sNum = document.querySelector(".seconds-container .number");
+        mNum = document.querySelector(".minutes-container .number");
+        hNum = document.querySelector(".hours-container .number");
+        dNum = document.querySelector(".days-container .number");
+        var interval = setInterval(updateClock, 1000);
+    };
+
 })();
